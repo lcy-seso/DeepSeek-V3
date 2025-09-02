47e30aef4a4972541d9cc247d4025a3c95559291
yuxiao.guo <yuxiao.guo@aigcic.com>
2025-09-02 19:04:16 +0800

    On-going

--- inference/func_test.py
+++ inference/func_test.py
@@ -21,14 +21,25 @@ def convert_state_dict(tilert_state_dict: dict) -> dict:
     Convert the state dict of the Tilert DeepSeekV3 model to the state dict of the DeepSeekV3 model.
     """
     key_casting_maps = {
+        # RMSNorm weight
         r"layers\.(\d+)\.ffn\.norm\.weight": r"layers.\1.ffn_norm.weight",
         r"layers\.(\d+)\.ffn\.norm_up_gate.norm_weight": r"layers.\1.ffn_norm.weight",  # noqa: E501
         r"layers\.(\d+)\.ffn\.route_proj\.norm_weight": r"layers.\1.ffn_norm.weight",  # noqa: E501
+        r"layers\.(\d+)\.ffn\.routed_gate\.norm_weight": r"layers.\1.ffn_norm.weight",  # noqa: E501
+        # MLP weight
         r"layers\.(\d+)\.ffn\.norm_up_gate.w1.weight": r"layers.\1.ffn.w1.weight",  # noqa: E501
         r"layers\.(\d+)\.ffn\.norm_up_gate.w3.weight": r"layers.\1.ffn.w3.weight",  # noqa: E501
         r"layers\.(\d+)\.ffn\.norm_up_gate.w1.scale": r"layers.\1.ffn.w1.scale",  # noqa: E501
         r"layers\.(\d+)\.ffn\.norm_up_gate.w3.scale": r"layers.\1.ffn.w3.scale",  # noqa: E501
-        r"layers\.(\d+)\.ffn\.route_proj\.proj_weight": r"layers.\1.ffn.gate.weight",  # noqa: E501
+        # Project weight
+        r"layers\.(\d+)\.ffn\.routed_gate\.proj_weight": r"layers.\1.ffn.gate.weight",  # noqa: E501
+        # Expert weight
+        r"layers\.(\d+)\.ffn\.experts_w1\.(\d+)\.weight": r"layers.\1.ffn.experts.\2.w1.weight",  # noqa: E501
+        r"layers\.(\d+)\.ffn\.experts_w1\.(\d+)\.scale": r"layers.\1.ffn.experts.\2.w1.scale",  # noqa: E501
+        r"layers\.(\d+)\.ffn\.experts_w2\.(\d+)\.weight": r"layers.\1.ffn.experts.\2.w2.weight",  # noqa: E501
+        r"layers\.(\d+)\.ffn\.experts_w2\.(\d+)\.scale": r"layers.\1.ffn.experts.\2.w2.scale",  # noqa: E501
+        r"layers\.(\d+)\.ffn\.experts_w3\.(\d+)\.weight": r"layers.\1.ffn.experts.\2.w3.weight",  # noqa: E501
+        r"layers\.(\d+)\.ffn\.experts_w3\.(\d+)\.scale": r"layers.\1.ffn.experts.\2.w3.scale",  # noqa: E501
     }
 
     state_dict = {}
@@ -52,14 +63,14 @@ def test_e2e_forward_pass(model_config):
         model_config = json.load(f_json)
     model_args = ModelArgs(**model_config)
 
-    x = torch.randint(0, model_args.vocab_size, (2, 128))
+    x = torch.randint(0, model_args.vocab_size, (1, 1))
 
     tilert_model = TilertDeepSeekV3Transformer(model_args)
     origin_model = DeepSeekV3Transformer(model_args)
     origin_model.load_state_dict(convert_state_dict(tilert_model.state_dict()))
 
-    ref_output = origin_model(x)
-    tilert_output = tilert_model(x)
+    ref_output = origin_model(x, start_pos=127)
+    tilert_output = tilert_model(x, start_pos=127)
 
     logit_sum = torch.sum(ref_output)
     logit_sum_tilert = torch.sum(tilert_output)
