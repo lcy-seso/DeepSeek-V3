17855988dec488625fafc94da098d31988e025f5
yuxiao.guo <yuxiao.guo@aigcic.com>
2025-09-02 15:21:50 +0800

    Add unitt test script

---
+++ inference/func_test.py
@@ -0,0 +1,79 @@
+import pytest
+import torch
+import re
+
+import json
+from model import ModelArgs
+
+from model import Transformer as DeepSeekV3Transformer
+from tilert.models.deepseek_v3.model import Transformer as TilertDeepSeekV3Transformer
+
+
+@pytest.fixture(autouse=True)
+def setup():
+    torch.set_default_dtype(torch.bfloat16)
+    torch.set_default_device("cuda")
+    torch.manual_seed(0)
+
+
+def convert_state_dict(tilert_state_dict: dict) -> dict:
+    """
+    Convert the state dict of the Tilert DeepSeekV3 model to the state dict of the DeepSeekV3 model.
+    """
+    key_casting_maps = {
+        r"layers\.(\d+)\.ffn\.norm\.weight": r"layers.\1.ffn_norm.weight",
+        r"layers\.(\d+)\.ffn\.norm_up_gate.norm_weight": r"layers.\1.ffn_norm.weight",  # noqa: E501
+        r"layers\.(\d+)\.ffn\.route_proj\.norm_weight": r"layers.\1.ffn_norm.weight",  # noqa: E501
+        r"layers\.(\d+)\.ffn\.norm_up_gate.w1.weight": r"layers.\1.ffn.w1.weight",  # noqa: E501
+        r"layers\.(\d+)\.ffn\.norm_up_gate.w3.weight": r"layers.\1.ffn.w3.weight",  # noqa: E501
+        r"layers\.(\d+)\.ffn\.norm_up_gate.w1.scale": r"layers.\1.ffn.w1.scale",  # noqa: E501
+        r"layers\.(\d+)\.ffn\.norm_up_gate.w3.scale": r"layers.\1.ffn.w3.scale",  # noqa: E501
+        r"layers\.(\d+)\.ffn\.route_proj\.proj_weight": r"layers.\1.ffn.gate.weight",  # noqa: E501
+    }
+
+    state_dict = {}
+    for key, value in tilert_state_dict.items():
+        is_found = False
+        for pattern, target_pattern in key_casting_maps.items():
+            match = re.match(pattern, key)
+            if match:
+                new_key = re.sub(pattern, target_pattern, key)
+                state_dict[new_key] = value
+                is_found = True
+                break
+        if not is_found:
+            state_dict[key] = value
+    return state_dict
+
+
+@pytest.mark.parametrize("model_config", [])
+def test_e2e_forward_pass(model_config):
+    with open(model_config, "r", encoding="utf-8") as f_json:
+        model_config = json.load(f_json)
+    model_args = ModelArgs(**model_config)
+
+    x = torch.randint(0, model_args.vocab_size, (2, 128))
+
+    tilert_model = TilertDeepSeekV3Transformer(model_args)
+    origin_model = DeepSeekV3Transformer(model_args)
+    origin_model.load_state_dict(convert_state_dict(tilert_model.state_dict()))
+
+    ref_output = origin_model(x)
+    tilert_output = tilert_model(x)
+
+    logit_sum = torch.sum(ref_output)
+    logit_sum_tilert = torch.sum(tilert_output)
+    print(logit_sum, logit_sum_tilert)
+
+    assert torch.allclose(ref_output, tilert_output)
+
+
+def main():
+    torch.set_default_device("cuda")
+    torch.set_default_dtype(torch.bfloat16)
+    torch.manual_seed(0)
+    test_e2e_forward_pass("configs/config_16B_v3.1.json")
+
+
+if __name__ == "__main__":
+    main()
